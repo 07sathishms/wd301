0ba68bf82d695b50cc6650744620681b01f58007
07sathishms <91666613+07sathishms@users.noreply.github.com>
2023-07-28 23:49:28 +0530

    Add files via upload

---
+++ hello-react/src/App.jsx
@@ -0,0 +1,30 @@
+import TaskCard from "./TaskCard";
+
+function App(){
+  return(
+    <div className="ml-20 flex items-center">
+      <div className="text-xl  rounded-md border-gray-400 bg-slate-100"> 
+         <h1>Pending</h1>
+         <TaskCard title="Build the website with static content" 
+         dueDate="Due on: 10th April"
+         assigneeName="Assignee: Rohit S"/>
+         <TaskCard title="Add a blog"
+          dueDate="Due on: 22nd March"
+          assigneeName="Assignee: Rohit M"/>
+         </div>
+    <div className="ml-20 text-xl rounded-md border-gray-400 bg-slate-100">
+      <h1>Done</h1>
+      <TaskCard title="Design the mockup"
+      completedAtDate="Completed on: 10th April"
+      assigneeName="Assignee: Rohit M"
+
+      />
+      <TaskCard title="Get the approval from principal"
+       completedAtDate="Completed on: 20th April"
+       assigneeName="Assignee: Ajay S"/>
+         
+    </div>
+      </div>
+  )
+}
+export default App;
---
+++ hello-react/src/TaskCard.jsx
@@ -0,0 +1,14 @@
+import './TaskCard.css'
+const TaskCard = (props) => {
+  console.log(props)
+  return (
+    <div className='TaskItem'>
+      <h2 className="text-xl font-bold">{props.title}</h2>
+      <p>{props.dueDate}</p>
+      <p>{props.completedAtDate}</p>
+      <p>{props.assigneeName}</p>
+    </div>
+  )
+}
+
+export default TaskCard;
