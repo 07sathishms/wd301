be7a9f1dccf9a98fe3f1baf6ca7e4bbea9386e88
07sathishms <91666613+07sathishms@users.noreply.github.com>
2023-08-20 18:32:01 +0530

    commit

---
+++ smarter-tasks/src/TaskList.tsx
@@ -0,0 +1,23 @@
+import Task from "./Task";
+import { TaskItem } from "./types";
+interface Props {
+  tasks: TaskItem[]
+  removeTask: (id: number) => void
+}
+const TaskList = (props: Props) => {
+
+  const { tasks, removeTask } = props
+
+  return tasks.map((task, idx) => (
+    <li className="list-none">
+      <Task
+  key={idx}
+  task={task}
+  id={idx.toString()}
+  removeTask={() => removeTask(idx)}
+/>
+
+    </li>
+  ));
+};
+export default TaskList;
